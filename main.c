ad11f75a853673931597a2ee15184027afeac7d9
taru.rata <saycheese5963@yahoo.co.jp>
2019-07-13 21:59:33 +0900

    ex7-2.c p115

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #define ARRAY_SIZE 13
 
-int linear_search (int array[], int n, int key)
+int binary_search ( int array[], int num, int key)
 {
-    int i;
-    for (i = 0; i < n; i++) {
-        if (array[i] == key) {
-            return i;
+    int middle, low, high;
+    low = 0;
+    high = num - 1;
+    while (low <= high) {
+        middle = (low + high) / 2;
+        if ( key == array[middle]) {
+            return middle;
+        }
+        else if (key < array[middle]) {
+            high = middle - 1;
+        }
+        else {
+            low = middle + 1;
         }
     }
     return -1;
@@ -17,26 +27,27 @@ void print_array (int array[], int n)
 {
     int i;
     for (i = 0; i < n; i++) {
-        printf( "%d ", array[i]);
+        printf("%d ", array[i]);
     }
     printf("\n");
 }
 
-int main ()
+int main()
 {
     int index, key;
     int array[ARRAY_SIZE] = {
-            900, 990, 210, 50, 80, 150, 330,
-            470, 510, 530, 800, 250, 280
+            50, 80, 150, 210, 250, 280, 330,
+            470, 510, 530, 800, 900, 990
     };
+
     key = 800;
-    print_array(array, ARRAY_SIZE);
-    index = linear_search (array, ARRAY_SIZE, key);
+    print_array( array, ARRAY_SIZE);
+    index = binary_search(array, ARRAY_SIZE, key);
     if (index != -1) {
-        printf("Found: %d (Index:%d)\n", key, index);
+        printf("Found: %d ( Index:%d)\n", key, index);
     }
     else {
-        printf( "Not Found: %d\n", key);
+        printf("Not Found: %d\n", key);
     }
     return 0;
 }
