78f870e33c52e4bd230fb4ed4031cf14670c9710
tarurata <saycheese5963@yahoo.co.jp>
2019-07-15 00:56:13 +0900

    ex8-2.c p127

--- main.c
+++ main.c
@@ -2,67 +2,59 @@
 #include <stdlib.h>
 
 #define MAX 128
+#define ENQUEUE_SUCCESS 1
+#define ENQUEUE_FAILURE -1
+#define DEQUEUE_SUCCESS 2
+#define DEQUEUE_FAILURE -2
 
-// 各リターンコードを設定
-#define PUSH_SUCCESS 1
-#define PUSH_FAILURE -1
-#define POP_SUCCESS 2
-#define POP_FAILURE -2
-
-// ポインタ変数topのアドレス先の値(実体)を初期化
-void stack_init (int *top)
+void queue_init (int *front, int *rear)
 {
-    *top = 0;
+    *front = -1;
+    *rear = -1;
 }
 
-void display (int stack[], int top)
+int enqueue (int q[], int *rear, int data)
 {
-    int i;
-    printf("STACK(%d): ", top);
-    for (i = 0; i < top; i++) {
-        printf("%d ", stack[i]);
-    }
-    printf("\n");
-}
-
-int push (int stack[], int *top, int data)
-{
-    if (*top >= MAX) {
-        return PUSH_FAILURE;
+    if (*rear < MAX - 1) {
+        *rear = *rear + 1;
+        q[*rear] = data;
+        return ENQUEUE_SUCCESS;
     }
     else {
-        stack[*top] = data;
-        (*top)++;
-        return PUSH_SUCCESS;
+        return ENQUEUE_FAILURE;
     }
 }
 
-int pop (int stack[], int *top, int *data)
+int dequeue (int q[], int *front, int rear, int *data)
 {
-    if ((*top) > 0) {
-        *data = stack[(*top) - 1];
-        (*top)--;
-        return POP_SUCCESS;
+    if (*front == rear) {
+        return DEQUEUE_FAILURE;
     }
-    else {
-        return POP_FAILURE;
-    }
+    *front = *front + 1;
+    *data = q[*front];
+    return DEQUEUE_SUCCESS;
 }
 
-int main ()
+int main()
 {
-    int stack[MAX];
-    int top, data;
-    int i;
+    int queue[MAX];
+    int front, rear, data;
+    int stat;
 
-    stack_init(&top);
-    for (i=0; i < 3; i++){
-        data = ((i+1) * 100) + 200;
-        printf("push: %d\n", data);
-        push (stack, &top, data);
+    queue_init (&front, &rear);
+    enqueue(queue, &rear, 100);
+    enqueue(queue, &rear, 200);
+    enqueue(queue, &rear, 300);
+    enqueue(queue, &rear, 400);
+    enqueue(queue, &rear, 500);
+    while (rear - front) {
+        stat = dequeue(queue, &front, rear, &data);
+        if (stat == DEQUEUE_SUCCESS){
+            printf("%d\n", data);
+        }
+        else {
+            printf("QUEUE is empty \n");
+        }
     }
-    for (i=0; i < 3; i++){
-        pop (stack, &top, &data);
-        printf("pop: %d\n", data);
-    }
+    return 0;
 }
