674cf2be3b620bd2843a68b9829fafc2949f4785
tarurata <saycheese5963@yahoo.co.jp>
2019-07-21 21:24:55 +0900

    ex11-1.c p169

--- main.c
+++ main.c
@@ -1,41 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_array (int v[], int n)
+void print_array(int v[], int n)
 {
     int i;
     printf("array: ");
-    for (i = 0; i<n; i++){
-        printf("%d ", v[i]);
+    for (i=0; i<n; i++){
+        for (i=0; i<n; i++){
+            printf("%d ", v[i]);
+        }
+        printf("\n");
     }
-    printf("\n");
 }
 
-// v=配列 n=配列の要素数
-void insertion_sort (int v[], int n)
+int partition(int v[], int lower_bound, int upper_bound)
 {
-    int i, j, t;
-    for (i=1; i<n; i++){
-        // 最初、1つめの要素を選択
-        j = i;
-        while ((j>=1) && (v[j-1]>v[j])){
-            // tは一時的に今評価されている要素の値を入れておくための変数(temporary)
-            // ここでv[0]がv[1]より大きかったら、v[0]の値とv[1]の値を入れ替える
-            t = v[j];
-            v[j] = v[j-1];
-            v[j-1] = t;
-            j--;
-            printf("i:%d j:%d  ", i, j);
-            print_array(v, n);
-        }}
+    int a, down, up, temp;
+    a = v[lower_bound];
+    up = upper_bound;
+    down = lower_bound;
+
+    while(down < up){
+        while((v[down] <= a) && (down < upper_bound)){
+            down++;
+        }
+        while(v[up] > a) {
+            up--;
+        }
+        if (down < up) {
+            temp = v[down];
+            v[down] = v[up];
+            v[up] = temp;
+        }
+    }
+    v[lower_bound] = v[up];
+    v[up] = a;
+    return up;
+}
+
+void quicksort (int v[], int left, int right)
+{
+    int p;
+    if (left >= right) {
+        return ;
+    }
+    // 分割
+    p = partition(v, left, right);
+    // 再帰関数呼び出し
+    quicksort(v, left, p-1);
+    // 再帰関数呼び出し
+    quicksort(v, p+1, right);
 }
 
 int main()
 {
-    int array[5]
-    = { 30, 20, 50, 10,40};
-    print_array(array, 5);
-    insertion_sort(array, 5);
-    print_array(array, 5);
+    int array[10]
+    = { 80, 40, 30, 20, 10, 00, 70, 90, 50, 60};
+
+    print_array(array, 10);
+    quicksort(array, 0, 9);
+    print_array(array, 10);
+
     return 0;
 }
