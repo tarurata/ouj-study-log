d2b08cb1c5c5f67f24dca065091f45e46ebbc0b4
tarurata <saycheese5963@yahoo.co.jp>
2019-07-21 17:51:18 +0900

    ex10-2.c p159

--- main.c
+++ main.c
@@ -1,39 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_array(int v[], int n)
+void print_array (int v[], int n)
 {
     int i;
-    printf ("array: ");
-    for (i=0; i<n; i++){
+    printf("array: ");
+    for (i = 0; i<n; i++){
         printf("%d ", v[i]);
     }
     printf("\n");
 }
 
-void bubble_sort (int v[], int n)
+// v=配列 n=配列の要素数
+void selection_sort (int v[], int n)
 {
-    int i, j, t;
-    for (i=0; i<n-1; i++){
-        for (j=n-1; j>i; j--){
-            if (v[j-1]>v[j]){
-                t = v[j];
-                v[j] = v[j - 1];
-                v[j - 1] = t;
+    // i=配列の要素数, j=全要素の中で、ソートに利用するループの回数、t=未ソート部分の先頭の要素
+    int i, j, t, min_index;
+    // まだソートされていない要素の中でループ
+    for (i =0;i<n-1; i++){
+        min_index = i;
+        // 最小の要素を探索
+        for(j=i+1; j<n; j++){
+            if (v[j] < v[min_index]){
+                min_index = j;
             }
             printf("i:%d j:%d  ", i, j);
             print_array(v, n);
         }
+        // 最小となった要素を未ソート部分の先頭の要素と交換
+        t=v[i];
+        v[i] = v[min_index];
+        v[min_index] = t;
     }
 }
 
 int main()
 {
     int array[5]
-    = {30, 50, 20, 10, 40};
+    = { 30, 50, 20, 10,40};
     print_array(array, 5);
-    bubble_sort(array, 5);
+    selection_sort(array, 5);
     print_array(array, 5);
     return 0;
 }
-
