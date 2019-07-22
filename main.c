b475a271431d2a59c99f7546577992bb71424fcb
tarurata <saycheese5963@yahoo.co.jp>
2019-07-22 21:07:04 +0900

    ex13-2.c p203

--- main.c
+++ main.c
@@ -1,40 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define NOT_FOUND (-1)
+#define DATA_SIZE 6
+
+
 struct node
 {
     int data;
-    // 構造体nodeのメンバとしてポインタ変数nextを宣言
     struct node *next;
 };
 
-// 構造体nodeをNODE_TYPEと命名
 typedef struct node NODE_TYPE;
 
-void linked_list_print (NODE_TYPE * node)
+// 連結リストの先頭にノードを挿入
+void linked_list_insert_head (NODE_TYPE ** head, int data)
 {
-    while (NULL != node) {
-        printf("%d ", node->data);
-        node = node->next;
+    NODE_TYPE *new_node;
+    new_node = malloc(sizeof (NODE_TYPE));
+    new_node->data = data;
+
+    // headになにも値が入っていなければ
+    if (*head == NULL){
+        new_node->next = NULL;
+        *head = new_node;
     }
-    printf("\n");
+    // headに値が入っていた場合、
+    else {
+        // 追加した新しい
+        new_node->next = *head;
+        *head = new_node;
+    }
+}
+
+void linked_list_print (NODE_TYPE * head)
+{
+    printf("Linked_list [ ");
+    while (NULL != head) {
+        printf("%02d ", head-> data);
+        head = head->next;
+    }
+    printf("]\n");
 }
 
 int main()
 {
-    NODE_TYPE *node;
-    node = malloc(sizeof (NODE_TYPE));
-    node->data = 300;
-    node->next = malloc (sizeof(NODE_TYPE));
-    node->next->data = 400;
-    node->next->next = malloc(sizeof (NODE_TYPE));
-    node->next->next->data = 500;
-    node->next->next->next = malloc(sizeof(NODE_TYPE));
-    node->next->next->next->data = 600;
+    NODE_TYPE *head;
+    int i, data1;
 
-    // 末尾となるノードにNULLを設定
-    node->next->next->next->next = NULL;
-
-    // リンクドリストを出力
-    linked_list_print(node);
+    head = NULL;
+    for (i = 0; i<DATA_SIZE; i++){
+        data1 = (int) rand () % 100;
+        printf("inserting (head):");
+        printf("%02d\n", data1);
+        linked_list_insert_head(&head, data1);
+    }
+    linked_list_print(head);
     return 0;
 }
