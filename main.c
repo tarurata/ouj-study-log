a56a85cc86219b966549cfdca2b54f97524fd0d3
taru.rata <saycheese5963@yahoo.co.jp>
2019-07-22 13:13:13 +0900

    ex12-2.c p186

--- main.c
+++ main.c
@@ -1,59 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 10
-
-void print_array (int v[], int n)
-{
-    int i;
-
-    printf("array: ");
-    for (i = 0; i<n; i++){
-        printf("%4d ", v[i]);
-    }
-    printf("\n");
-}
-
-void radix_sort(int a[], int n)
-{
-    int i, max, exp;
-    int temp[MAX];
-    int bucket[10];
-
-    max = 0;
-    exp = 1;
-    for (i=0; i<n; i++){
-        if (a[i] > max){
-           max = a[i];
-        }
-    }
-    while (max/exp >0){
-        for (i=0; i<10; i++) {
-            bucket[i] = 0;
-        }
-        for (i=0; i<n; i++){
-            bucket[a[i]/ exp %10]++;
-        }
-        for (i=1; i<10; i++) {
-            bucket[i] += bucket[i-1];
-        }
-        for (i=n-1; i>=0; i--){
-            temp[--bucket[a[i]/ exp %10]] = a[i];
-        }
-        for (i=0; i<n; i++){
-            a[i] = temp[i];
-        }
-        exp *= 10;
-        print_array(a, n);
-    }
-}
+#define ARRAY_SIZE 3000000
+// グローバル変数はメモリ上データセグメントに保存される
+int array[ARRAY_SIZE];
 
 int main()
 {
-    int array[MAX]
-    = { 12, 19, 10, 28, 30, 01, 502, 16, 34, 177};
-    print_array(array, 10);
-    radix_sort(array, 10);
-    print_array(array, 10);
-
+    // ローカル変数はメモリ上スタックセグメント（スタックメモリ）に保存される
+    int i;
+    for (i=0; i<ARRAY_SIZE; i++){
+        array[i] = 100;
+    }
+    for (i=0; i<10; i++){
+        printf("%d ", array[i]);
+    }
     return 0;
 }
+
